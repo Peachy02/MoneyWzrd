276a6e77b0bb826aff2a942d6b42f2c2aa74fdb3
Peachy02 <daniel2lapiccirella@gmail.com>
2020-11-03 13:47:08 -0500

    first commit

---
+++ moneywzrd/src/main/java/ispy/corp/moneywzrd/MainActivity.java
@@ -0,0 +1,14 @@
+package ispy.corp.moneywzrd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
