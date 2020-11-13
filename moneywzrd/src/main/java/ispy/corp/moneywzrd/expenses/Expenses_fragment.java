0fcce1d432b7ed3ec0476624ca6e878c9c3faff3
Brandon Nicoll <n01338740@humbermail.ca>
2020-11-13 14:11:08 -0500

    started expenses page, designed the page and added a button to add expenses using a dialog popup.

--- moneywzrd/src/main/java/ispy/corp/moneywzrd/expenses/Expenses_fragment.java
+++ moneywzrd/src/main/java/ispy/corp/moneywzrd/expenses/Expenses_fragment.java
@@ -2,6 +2,8 @@ package ispy.corp.moneywzrd.expenses;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,12 +13,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import ispy.corp.moneywzrd.R;
 
 public class Expenses_fragment extends Fragment { //brandon nicoll
 
     private ExpensesFragViewModel mViewModel;
+    View rootView;
 
     public static Expenses_fragment newInstance() {
         return new Expenses_fragment();
@@ -25,8 +32,53 @@ public class Expenses_fragment extends Fragment { //brandon nicoll
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.expenses_fragment, container, false);
+
+        rootView = inflater.inflate(R.layout.expenses_fragment, container, false);
+
+        Button addExpenses = (Button)rootView.findViewById(R.id.addExpense);
+        TextView expense1 = (TextView) rootView.findViewById(R.id.expense1);
+        addExpenses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Enter Expense");
+
+                final EditText input = new EditText(getContext());
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                input.setLayoutParams(lp);
+                builder.setView(input);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        if (expense1.getText().toString().equals("")) {
+                            //String input = input.getText().toString();
+                            expense1.setText(input.getText().toString());
+                        }
+
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            }
+        });
+
+        return rootView;
     }
+
+    /*@Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        Button addExpenses = (Button)getActivity().findViewById(R.id.addExpense);
+    }
+
+     */
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
