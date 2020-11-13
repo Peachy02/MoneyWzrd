fa2ddd9774fd040cd082f33678ebe8f752815c5c
Brandon Nicoll <n01338740@humbermail.ca>
2020-11-13 15:00:23 -0500

    added a date picker to allow multiple inputs from user for each expense that will now also show the due date of the expense.

--- moneywzrd/src/main/java/ispy/corp/moneywzrd/expenses/Expenses_fragment.java
+++ moneywzrd/src/main/java/ispy/corp/moneywzrd/expenses/Expenses_fragment.java
@@ -1,9 +1,15 @@
 package ispy.corp.moneywzrd.expenses;
 
+import androidx.annotation.RequiresApi;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,13 +20,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import ispy.corp.moneywzrd.R;
 
-public class Expenses_fragment extends Fragment { //brandon nicoll
+public class Expenses_fragment extends Fragment { //brandon nicoll - n01338740
 
     private ExpensesFragViewModel mViewModel;
     View rootView;
@@ -34,28 +41,62 @@ public class Expenses_fragment extends Fragment { //brandon nicoll
                              @Nullable Bundle savedInstanceState) {
 
         rootView = inflater.inflate(R.layout.expenses_fragment, container, false);
-
+        final DatePickerDialog.OnDateSetListener[] mDateSetListener = {null};
         Button addExpenses = (Button)rootView.findViewById(R.id.addExpense);
         TextView expense1 = (TextView) rootView.findViewById(R.id.expense1);
+        TextView date1 = (TextView) rootView.findViewById(R.id.date1);
         addExpenses.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
                 builder.setTitle("Enter Expense");
 
-                final EditText input = new EditText(getContext());
-                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.MATCH_PARENT,
-                        LinearLayout.LayoutParams.MATCH_PARENT);
-                input.setLayoutParams(lp);
-                builder.setView(input);
+                final EditText input1 = new EditText(getContext());
+                final TextView input2 = new TextView(getContext());
+                input2.setText(" Click to select due date");
+                LinearLayout lila1= new LinearLayout(getContext());
+                lila1.setOrientation(LinearLayout.VERTICAL);
+                lila1.addView(input1);
+                lila1.addView(input2);
+                builder.setView(lila1);
+
+                input2.setOnClickListener(new View.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.N)
+                    @Override
+                    public void onClick(View view) {
+                        Calendar cal = Calendar.getInstance();
+                        int year = cal.get(Calendar.YEAR);
+                        int month = cal.get(Calendar.MONTH);
+                        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                        DatePickerDialog dialog = new DatePickerDialog(
+                                getContext(),
+                                android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                                mDateSetListener[0],
+                                year,month,day);
+                        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                        dialog.show();
+                    }
+                });
+                mDateSetListener[0] = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        month = month + 1;
+
+
+                        String date = month + "/" + day + "/" + year;
+                        input2.setText(date);
+                    }
+                };
+
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
                         if (expense1.getText().toString().equals("")) {
                             //String input = input.getText().toString();
-                            expense1.setText(input.getText().toString());
+                            expense1.setText(input1.getText().toString());
+                            date1.setText(input2.getText().toString());
                         }
 
                     }
