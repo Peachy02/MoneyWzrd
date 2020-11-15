9f804fc2c8b3d405b6d0ca9d04e20a1229ff786f
Brandon Nicoll <n01338740@humbermail.ca>
2020-11-14 22:07:37 -0500

    Linked login with register page, and declared references to ID's

--- moneywzrd/src/main/java/ispy/corp/moneywzrd/Login_main.java
+++ moneywzrd/src/main/java/ispy/corp/moneywzrd/Login_main.java
@@ -2,13 +2,31 @@ package ispy.corp.moneywzrd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class Login_main extends AppCompatActivity {
+public class Login_main extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_main);
+
+        register = (TextView)findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
+
     }
 }
--- moneywzrd/src/main/java/ispy/corp/moneywzrd/RegisterUser.java
+++ moneywzrd/src/main/java/ispy/corp/moneywzrd/RegisterUser.java
@@ -3,12 +3,39 @@ package ispy.corp.moneywzrd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class RegisterUser extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
+public class RegisterUser extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth mAuth;
+    private TextView banner, registerUser;
+    private EditText editTextFullName, editTextEmail, editTextPassword;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+
+        registerUser = (Button)findViewById(R.id.registerUser);
+        registerUser.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
