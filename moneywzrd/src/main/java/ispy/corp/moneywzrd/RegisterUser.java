076caa78e76af9dd968bb0f087a8cf12679c1fdf
Brandon Nicoll <n01338740@humbermail.ca>
2020-11-15 00:02:39 -0500

    Finished register page with firebase implementation but currently fails to save user data?

--- moneywzrd/src/main/java/ispy/corp/moneywzrd/RegisterUser.java
+++ moneywzrd/src/main/java/ispy/corp/moneywzrd/RegisterUser.java
@@ -89,23 +89,26 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
         }
 
         progressBar.setVisibility(View.VISIBLE);
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
 
                 if (task.isSuccessful()) {
                     User user = new User(fullName, email);
-                    FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    FirebaseDatabase.getInstance().getReference("Users")
+                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if (task.isSuccessful()) {
                                 Toast.makeText(RegisterUser.this, "User has been registered", Toast.LENGTH_LONG).show();
-                                progressBar.setVisibility(View.GONE);
                             }
                             else {
                                 Toast.makeText(RegisterUser.this, "Failed to register! Try Again!", Toast.LENGTH_LONG).show();
                                 progressBar.setVisibility(View.GONE);
                             }
+
                         }
                     });
                 }
