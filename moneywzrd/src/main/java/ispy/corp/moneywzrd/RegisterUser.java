af674d7879e74754efeb209ef0dd2202175ebe1e
Brandon Nicoll <n01338740@humbermail.ca>
2020-11-14 22:26:09 -0500

    Started validation of the different registration fields.

--- moneywzrd/src/main/java/ispy/corp/moneywzrd/RegisterUser.java
+++ moneywzrd/src/main/java/ispy/corp/moneywzrd/RegisterUser.java
@@ -2,7 +2,9 @@ package ispy.corp.moneywzrd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,8 +12,6 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
-
-import org.w3c.dom.Text;
 
 public class RegisterUser extends AppCompatActivity implements View.OnClickListener{
 
@@ -32,10 +32,45 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
 
         registerUser = (Button)findViewById(R.id.registerUser);
         registerUser.setOnClickListener(this);
+
+        editTextFullName = (EditText)findViewById(R.id.fullname);
+        editTextEmail = (EditText)findViewById(R.id.email);
+        editTextPassword = (EditText)findViewById(R.id.password);
+        progressBar = (ProgressBar)findViewById(R.id.progressBarR);
     }
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.banner:
+                startActivity(new Intent(this, Login_main.class));
+                break;
+            case R.id.registerUser:
+                registerUser();
+                break;
+        }
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String fullName = editTextFullName.getText().toString().trim();
+
+        if (fullName.isEmpty()) {
+            editTextFullName.setError("Full name is required!");
+            editTextFullName.requestFocus();
+            return;
+        }
+        if (email.isEmpty()){
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please provide valid email!");
+            editTextEmail.requestFocus();
+            return;
+        }
 
     }
 }
