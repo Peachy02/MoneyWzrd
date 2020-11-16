a250ec357e420cfd7ae460a92ea0317cd57ccc98
Brandon Nicoll <n01338740@humbermail.ca>
2020-11-16 13:51:47 -0500

    Added welcome message that access the users full name from the database, also the ability to access more of the users info in the future for more implementation.

--- moneywzrd/src/main/java/ispy/corp/moneywzrd/home/Home_fragment.java
+++ moneywzrd/src/main/java/ispy/corp/moneywzrd/home/Home_fragment.java
@@ -11,13 +11,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 import ispy.corp.moneywzrd.R;
+import ispy.corp.moneywzrd.User;
 
 public class Home_fragment extends Fragment {
 
     private HomeFragmentViewModel mViewModel;
     View rootView;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
 
     public static Home_fragment newInstance() {
         return new Home_fragment();
@@ -28,6 +44,28 @@ public class Home_fragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.home_fragment, container, false);
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        TextView welcome = (TextView)rootView.findViewById(R.id.welcomeName);
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userprofile = snapshot.getValue(User.class);
+                if (userprofile != null) {
+                    String fullName = userprofile.fullName;
+                    String email = userprofile.email;
+
+                    welcome.setText("Welcome back, " + fullName + "!");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), "Something went wrong!", Toast.LENGTH_LONG).show();
+            }
+        });
 
 
         return rootView;
