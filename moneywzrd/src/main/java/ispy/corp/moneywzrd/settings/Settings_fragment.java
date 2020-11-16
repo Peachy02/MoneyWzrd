f7c8c21324a62acf4fa36d3c5bda0b2ffaafec59
Brandon Nicoll <n01338740@humbermail.ca>
2020-11-16 13:33:10 -0500

    Added logout option to the settings page. Displays toast on success.

--- moneywzrd/src/main/java/ispy/corp/moneywzrd/settings/Settings_fragment.java
+++ moneywzrd/src/main/java/ispy/corp/moneywzrd/settings/Settings_fragment.java
@@ -2,6 +2,7 @@ package ispy.corp.moneywzrd.settings;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,12 +12,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import ispy.corp.moneywzrd.Login_main;
 import ispy.corp.moneywzrd.R;
 
 public class Settings_fragment extends Fragment {
 
     private SettingsFragmentViewModel mViewModel;
+    View rootView;
 
     public static Settings_fragment newInstance() {
         return new Settings_fragment();
@@ -25,7 +32,19 @@ public class Settings_fragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.settings_fragment, container, false);
+        rootView = inflater.inflate(R.layout.settings_fragment, container, false);
+        Button logout = (Button)rootView.findViewById(R.id.signOut);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(getContext(), "Successfully logged out!", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(getContext(), Login_main.class));
+            }
+        });
+
+
+        return rootView;
     }
 
     @Override
