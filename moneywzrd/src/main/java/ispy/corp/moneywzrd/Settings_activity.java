50d75e06c6dfa97b158952cad415ce568afcbf43
Brandon Nicoll <n01338740@humbermail.ca>
2020-12-03 17:50:39 -0500

    Back button in settings.

--- moneywzrd/src/main/java/ispy/corp/moneywzrd/Settings_activity.java
+++ moneywzrd/src/main/java/ispy/corp/moneywzrd/Settings_activity.java
@@ -2,8 +2,13 @@ package ispy.corp.moneywzrd;
 
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Settings_activity extends PreferenceActivity {
 
@@ -13,4 +18,19 @@ public class Settings_activity extends PreferenceActivity {
         //setContentView(R.layout.activity_settings_activity);
         addPreferencesFromResource(R.xml.prefs);
     }
+
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("Exit settings?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        startActivity(new Intent(Settings_activity.this, MainActivity.class));
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+
+    }
+
 }
